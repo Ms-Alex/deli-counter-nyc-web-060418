1948025c26379ab7a52f8fe9598ddf5364d6ce4d
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-16 04:43:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,7 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli == 0 
     puts "The line is currently empty."
-  else
-    katz_deli.each_with_index do |name, i|
-      
+  
 end
 
 def take_a_number
