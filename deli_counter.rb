d6e031852f14ba8269a7837b2e5b91b868215d01
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-16 04:49:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,7 @@ def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
   else
-    print "The line is currently: "
-    katz_deli.each_with_index
-  end
+
 end
 
 def take_a_number
